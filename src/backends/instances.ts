ca99717db7936900bb9cd26a8ea684936c786062
Anthony Porthouse <anthony.porthouse@arbor-education.com>
2023-03-07 21:46:15 +0000

    feat: add multimc support

--- src/backends/instances.ts
+++ src/backends/instances.ts
@@ -1,9 +1,11 @@
 import { getMinecraftInstances } from "./minecraft";
+import { getMultiMcInstances } from "./multimc";
 import { getPrismInstances } from "./prism";
 
 export function getInstances(): string[] {
     const instances: string[] = [
         ...getMinecraftInstances(),
+        ...getMultiMcInstances(),
         ...getPrismInstances(),
     ];
 
