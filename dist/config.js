f96e1f5b6e33d02ab7b7d78e3b2b771ebefeb90c
Anthony Porthouse <anthony.porthouse@arbor-education.com>
2023-03-07 09:51:01 +0000

    build: only include dist in publish

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cosmiconfig_1 = require("cosmiconfig");
-const explorer = (0, cosmiconfig_1.cosmiconfigSync)('mc-scripts');
-const config = explorer.search();
-exports.default = config;
