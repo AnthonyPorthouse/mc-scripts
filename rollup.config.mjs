0ec68587734abfe0c522528e94b3f0ef69cb81f1
Anthony Porthouse <anthony.porthouse@arbor-education.com>
2023-03-09 11:03:34 +0000

    fix: bundling of application

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import shebang from 'rollup-plugin-preserve-shebang'
+import shebang from 'rollup-plugin-preserve-shebang';
+import executable from 'rollup-plugin-executable'
 
 export default {
     input: 'src/app.ts',
@@ -10,5 +10,5 @@ export default {
         file: 'dist/app.js',
         format: 'cjs',
     },
-    plugins: [typescript(), json(), shebang(), commonjs(), nodeResolve()]
+    plugins: [ commonjs(), typescript(), json(), shebang(), executable()]
 }
